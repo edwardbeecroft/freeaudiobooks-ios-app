de15c8e5f3071b4a697578a28176642ba7873c8b
Ed Beecroft <edward.beecroft@gmail.com>
2026-09-01 20:40:28 +0100

    Update covers in email prompt.

--- FreeAudiobooks/View/EmailOptIn/EmailOptInPromptVC.swift
+++ FreeAudiobooks/View/EmailOptIn/EmailOptInPromptVC.swift
@@ -223,7 +223,7 @@ final class EmailOptInPromptVC: BottomSheetController {
         case .mystery: return ["mystery-1", "mystery-15", "mystery-16"]
         case .fantasy: return ["fantasy-1", "fantasy-13", "fantasy-12"]
         case .adventure: return ["adventure-14", "adventure-18", "adventure-17"]
-        case .historical: return ["historical-13", "historical-12", "historical-3"]
+        case .historical: return ["historical-17", "historical-15", "historical-16"]
         case .scienceFiction: return ["scifi-1", "scifi-12", "scifi-3"]
         case .horror: return ["horror-3", "horror-12", "horror-1"]
         case .comedy: return ["comedy-12", "comedy-4", "comedy-11"]
