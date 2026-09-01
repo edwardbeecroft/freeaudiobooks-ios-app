e1e80117462bb77b69efb0164423732b1edd0f76
Ed Beecroft <edward.beecroft@gmail.com>
2026-09-01 15:55:55 +0100

    Update covers in onboarding.

--- FreeAudiobooks/View/NewOnboarding/Components/PersonalizedPicksAnimationView.swift
+++ FreeAudiobooks/View/NewOnboarding/Components/PersonalizedPicksAnimationView.swift
@@ -87,7 +87,7 @@ struct PersonalizedPicksAnimationView: View {
         case .mystery: return ["mystery-12", "mystery-14", "mystery-11"]
         case .fantasy: return ["fantasy-14", "fantasy-16", "fantasy-3"]
         case .adventure: return ["adventure-15", "adventure-11", "adventure-13"]
-        case .historical: return ["historical-11", "historical-2", "historical-12"]
+        case .historical: return ["historical-11", "historical-12", "historical-14"]
         case .scienceFiction: return ["scifi-1", "scifi-2", "scifi-3"]
         case .horror: return ["horror-1", "horror-2", "horror-4"]
         case .comedy: return ["comedy-1", "comedy-2", "comedy-3"]
